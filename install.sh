993157790725bb72922da56a73efdcfca4bf2348
Xivo <lai.xavier3108@gmail.com>
2025-11-03 13:39:24 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -124,6 +124,6 @@ echo "Les commandes du jeu (statut, regarder, etc.) sont dans le dossier 'script
 echo
 echo "Tapez la commande suivante pour commencer :"
 echo
-echo "  export PATH=\$(pwd)/scripts:\$PATH && cd DONJON/salles/entree"
+echo "chmomd +x scripts/* && export PATH=\$(pwd)/scripts:\$PATH && cd DONJON/salles/entree"
 echo
 echo "###################################################################"
